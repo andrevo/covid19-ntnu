edc70c0b3f978968e668d68a6bf1106c4b60a4db
Erlend Magnus Lervik Coates <emcoates@ntnu.no>
2020-05-13 12:04:34 +0200

    Added functionality to let number of infected rise in the beginning

--- Network models/Master model/generateIDData_testing.py
+++ Network models/Master model/generateIDData_testing.py
@@ -4,6 +4,49 @@ import numpy
 import os
 import sys
 
+def run_strat_days_delay(strat,t,days):
+    folder = "covid19-ntnu/Network models/Master model/"
+    layers, attrs = initModel(folder + 'idAndAge_Trondheim.txt', folder + 'socialNetwork_Trondheim.txt', '', baseP, [10, 3, -.75], 20)
+    #layers, attrs = initModel('idAndAge_Oslo.txt', 'socialNetwork_Oslo.txt', '', baseP, [10, 3, -.75], 20)
+
+    curDay = 0
+    runDays = days
+
+    cont = 1
+    i = curDay
+    startStrat = {'S': 20, 'W': 1.0, 'R': 3}
+    p = setStrategy(startStrat, baseP, layers, attrs)
+
+    stateLog = []
+    infLog = []
+    infLogByLayer = []
+    endDay = curDay + runDays
+
+    testing = {}
+    testRules = setTestRules(testing, layers, attrs)
+
+    while cont and (i < endDay):
+        i += 1
+        sys.stdout.flush()
+        sys.stdout.write(str(i) + '\r')
+
+        dailyInfs = 0
+
+        cont, linfs, dailyInfs = systemDay(layers, attrs, p, i, testRules)
+        print linfs
+        count = countState(attrs, stateList)
+        stateLog.append(count)
+        infLog.append(dailyInfs)
+        infLogByLayer.append(linfs)
+
+        if count['Is'] > 500:
+            p = setStrategy(strat, baseP, layers, attrs)
+            testing = {'testStrat': 'TPHT', 'capacity': t, 'cutoff': 3}
+            testRules = setTestRules(testing, layers, attrs)
+
+    return stateLog
+
+
 def run_strat_days(strat,t,days):
     folder = "covid19-ntnu/Network models/Master model/"
     layers, attrs = initModel(folder + 'idAndAge_Trondheim.txt', folder + 'socialNetwork_Trondheim.txt', '', baseP, [10, 3, -.75], 20)
@@ -15,7 +58,8 @@ def run_strat_days(strat,t,days):
 
 def run_strat_and_save(r,t,k,maxDays):
     strat = {'S': 20, 'W': 1.0, 'R': r}
-    stateLog = run_strat_days(strat,t,maxDays)
+    stateLog = run_strat_days_delay(strat,t,maxDays)
+    #stateLog = run_strat_days(strat, t, maxDays)
     data = {}
     for comp in stateList:
         data[comp] = numpy.array([d[comp] for d in stateLog])
