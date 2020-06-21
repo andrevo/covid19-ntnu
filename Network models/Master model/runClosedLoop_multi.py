efca558d3a3de0b2e70acd99fa99783a5da00bbd
Ubuntu <ubuntu@almaaslab-iaas-covid1.novalocal>
2020-06-21 23:34:05 +0000

    Updated main closed loop simulation file

--- Network models/Master model/runClosedLoop_multi.py
+++ Network models/Master model/runClosedLoop_multi.py
@@ -8,17 +8,14 @@ import multiprocessing
 import os
 
 def controlledRun(simDays,k):
-    layers, seedAttrs, cliques = initModel('idAndAge_Trondheim.txt', 'socialNetwork_Trondheim.txt', '', baseP, [10, 3, -.75], 20)
+    layers, seedAttrs = initModel('idAndAge_Trondheim.txt', 'socialNetwork_Trondheim.txt', '', baseP, [10, 3, -.75], 20)
     cont = 1
     i = 0
     
-    strats = [{'S': 0, 'W': 0, 'R': 1}, {'S': 1, 'W': 0, 'R': 1}, {'S': 1, 'W': 0, 'R': 2}, {'S': 1, 'W': 1, 'R': 2},
-              {'S': 1, 'W': 1, 'R': 3}]  # Schools
-
     stateLog = []
     infLog = []
     infLogByLayer = []
-    attrs = copy.copy(seedAttrs)
+    attrs = copy.copy(seedAttrs) 
 
     eng = matlab.engine.start_matlab()
     eng.addpath(r'/home/ubuntu/software/casadi')
@@ -29,99 +26,104 @@ def controlledRun(simDays,k):
     N = 213999.0
     eng.workspace['N'] = N
     # Max ICU capacity [num individuals]
-
-    ICU_max = 50.0
-    if (k<8):
-        ICU_max = 50
-    elif (k < 16):
-        ICU_max = 100
-    elif (k < 24):
-        ICU_max = 150
-    else:
-        ICU_max = 200
-
+    ICU_max = 74.0
+    H_max   = 270.0 # this actually includes ICU.. correct for this in next iteration..
     
     eng.workspace['ICU_max'] = ICU_max
+    eng.workspace['H_max']   = H_max;
+    eng.workspace['safety_factor'] = 0.7
 
     # Run init script
-    n_pade = 3
-    eng.workspace['n_pade'] = matlab.double([n_pade])
-    eng.init_SI_ICU_pade(nargout=0)
-    eng.eval('opt.horizon = 80;', nargout=0)
-    eng.eval('opt.integer = 0;', nargout=0)
+    eng.eval('opt.integer = 0;', nargout=0) # NB: need to come before init script
+    eng.init_SIRHICU(nargout=0)
+    #252
+    eng.eval('opt.horizon = 98;', nargout=0) # 22 weeks   NB: this should be long enough, especially when using mixed integer optimization
+    eng.eval('opt.RK4_steps = 2;',nargout=0)  # TODO: test what is the best value here..
 
     # Sample time for discretization [days]
-    dt_u = 10.0
+    dt_u = 7.0 # 7days/1 week
     eng.workspace['dt_u'] = dt_u
 
-    # State [fraction of total population]
-    p_S_0 = (N - 20.0) / N
-    p_I_0 = 20.0 / N
-
     # NB! this is used from day 0 to day 1!
-    u = 4.0  # assume no control at start
+    u = matlab.double([[0.0], [0.0], [0.0]])  # assume full control at start
 
     # Disturbance estimate (integral action)
-    d_est = 0.0
-    k_I = 0.0#25.0
+    # NB: currently not implemented!
+    d_I = 0.0
+    d_H = 0.0
+    d_ICU = 0.0
+    k_I   = -0.1
+    k_H   = -0.01
+    k_ICU = -0.01
+    I_pred_last = 0.0
+    H_pred_last = 0.0
+    ICU_pred_last = 0.0
 
-    # Initial condition for actual pade states
-    z_0 = []
-    for l in range(n_pade):
-        z_0.append([0])
-    eng.workspace['z_0'] = matlab.double(z_0)
+    H_dot = 0.0
+    ICU_dot = 0.0
+    H_LP = 0.0
+    ICU_LP = 0.0
+
+    LPs_out = []
+    dots_out = []
+
+    T_H = 0.1
+    T_ICU = 0.1
 
     count = countState(attrs, stateList)
 
-    d_est_out = []
-    d_est_out.append(d_est)
+    d_out = []
     u_round_out = []
     u_raw_out = []
-    ICU_pred= []
-    S_preds = []
-    I_preds = []
-    z1_preds = []
-    z2_preds = []
-    z3_preds = []
+
+    x_preds = []
+    u_preds = []
+    slack_vars = []
 
     while cont and (i < simDays):
-        I = float(count['Ia'] + count['Ip']) / N 
+        S = float(count['S']) / N
+        I = float(count['Ia'] + count['Ip']) / N
+        H = float(count['H']) / N
+        ICU = float(count['ICU']) / N
 
         if (i % dt_u == 1):
-            S = float(count['S']) / N
-            
-            ICU = float(count['ICU']) / N
+            if(i>1):
+                # Update disturbance estimates
+                d_I   += k_I   * (I_pred_last   - I)
+                d_H   += k_H   * (H_pred_last   - H)
+                d_ICU += k_ICU * (ICU_pred_last - ICU)
 
-            x = [[S], [I]]
+            x = [[S], [I], [H], [ICU]]
             eng.workspace['x'] = matlab.double(x)
+            #print x
 
+            #d_est = matlab.double([[d_I],[d_H],[d_ICU]])
+            d_est = matlab.double([[0.0],[0.0],[0.0]])
+            print d_est
             eng.workspace['d_est'] = d_est
-            eng.eval('model.dyn_fun = @(x,u) dynamics_SIR_ICU_d_1(x,u,d_est,params,model.beta_fun,n_pade);', nargout=0)
+            eng.eval("model.dyn_fun = @(x,u) dynamics_SIRHICU(x,u',d_est,params,model.school_fun);", nargout=0)
+            eng.workspace['u_prev'] = u
 
-            eng.workspace['u_prev'] = round(u)
+            eng.workspace['z_H_0'] = H_dot;
+            eng.workspace['z_ICU_0'] = ICU_dot;
 
-            [u,info] = eng.eval('step_nmpc([x;z_0],u_prev,dt_u,model,objective,opt);',nargout=2)
-            #u = eng.eval('step_nmpc([x;zeros(n_pade,1)],u_prev,dt_u,model,objective,opt);',nargout=2)
+            [u,info] = eng.eval('step_nmpc([x;z_H_0;z_ICU_0],u_prev,dt_u,model,objective,opt);',nargout=2)
+            #print u
 
-            S_preds.append(info["x_opt"][0])
-            I_preds.append(info["x_opt"][1])
-            z1_preds.append(info["x_opt"][2])
-            z2_preds.append(info["x_opt"][3])
-            z3_preds.append(info["x_opt"][4])
-
-            e_y = 0.9 * ICU_max / N - ICU
-            d_est += k_I * e_y
-
-            #e_pred_out.append(N * e_y)
-            d_est_out.append(d_est)
- 
+            x_preds.append(info["x_opt"])
+            u_preds.append(info["u_opt"])
+            slack_vars.append(info["s_opt"])
+            I_pred_last = info["x_opt"][1][1]
+            H_pred_last = info["x_opt"][2][1]
+            ICU_pred_last = info["x_opt"][3][1]
+ 	
+        LPs_out.append([H_LP,ICU_LP])
+        dots_out.append([H_dot,ICU_dot])
+        
         if(i>0):
-            eng.workspace['p_I'] = I
-            eng.eval('u_z = p_I*(params.c*p_I + params.d);', nargout=0)
-            eng.eval('z_0 = sim_z(params.A,params.B,u_z,z_0);', nargout=0) # NB! This is open-loop. Use observer?
-
-            ICU_pred.append(eng.eval('params.C*z_0 + params.D*u_z'))
-
+            [H_dot,H_LP] = eng.filt_diff(H_LP,H,T_H,nargout=2)
+            [ICU_dot,ICU_LP] = eng.filt_diff(ICU_LP,ICU,T_ICU,nargout=2)
+        #print H_dot, I_LP, ICU_dot, H_LP
 
         i += 1
         print i
@@ -130,44 +132,50 @@ def controlledRun(simDays,k):
 
         dailyInfs = 0
 
-        u_rounded = int(round(u))
+        u1 = u[0][0] # NB: Need to transform this when doing mixed integer..
+        u2 = u[1][0]
+        u3 = u[2][0]
+        u1_rounded = int(round(u1))
+        u2_rounded = int(round(u2))
+        u3_rounded = int(round(u3))
 
-        u_round_out.append(u_rounded)
-        u_raw_out.append(u)
+        u_round_out.append([u1_rounded,u2_rounded,u3_rounded])
+        u_raw_out.append([u1,u2,u3])
+        d_out.append([d_I,d_H,d_ICU])
+        strat = {'S': u1_rounded, 'W': u2, 'R': u3_rounded}
+        #inVec = convertVector(strat)
+        
+        p = setStrategy(strat, baseP, layers, attrs)
 
-        strat = strats[u_rounded]
-        inVec = convertVector(strat)
-        openLayers, p = setStrategy(inVec, baseP, layers)
-
-        cont, linfs, dailyInfs = systemDay(cliques, attrs, openLayers, p, i)
+        testRules = {}
+        cont, linfs, dailyInfs = systemDay(layers, attrs, p, i, testRules)
         count = countState(attrs, stateList)
         stateLog.append(count)
         infLog.append(dailyInfs)
         infLogByLayer.append(linfs)
 
     data = {}
-    data['d_est'] = d_est_out
+    data['d_out'] = d_out
     data['u_raw'] = u_raw_out
     data['u_rounded'] = u_round_out
-    data['S_preds'] = S_preds
-    data['I_preds'] = I_preds
-    data['z1_preds'] = z1_preds
-    data['z2_preds'] = z2_preds
-    data['z3_preds'] = z3_preds
-    data['ICU_pred'] = ICU_pred
+    data['LPs']   = LPs_out
+    data['dots'] = dots_out
+    data['x_preds'] = x_preds
+    data['u_preds'] = u_preds
+    data['slack_vars'] = slack_vars
     for comp in stateList:
         data[comp] = numpy.array([d[comp] for d in stateLog])
 
-    dir = os.path.join(os.getcwd(),"cl_data")
+    dir = os.path.join(os.getcwd(),"new_control5")
     if not os.path.exists(dir):
         os.mkdir(dir)
-    fileName = 'cl_data/closedLoop_test_run' + str(k) + '.mat'
+    fileName = 'new_control5/closedLoop_test_run' + str(k) + '.mat'
     scipy.io.savemat(fileName,data)
 
 
-days = 10000
-num_cores = multiprocessing.cpu_count()
-N = num_cores
+days = 200
+num_cores = 10#multiprocessing.cpu_count()
+N = 10#num_cores
 Parallel(n_jobs=num_cores, verbose=10)(delayed(controlledRun)(days,k) for k in range(N))
 
 
